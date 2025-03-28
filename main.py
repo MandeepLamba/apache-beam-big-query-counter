eaeab9c91ca58d37847d225d1a9d9a435c81614a
MandeepLamba <mandeeplamba225@gmail.com>
2025-03-28 08:33:55 +0530

    updated input source

--- main.py
+++ main.py
@@ -1,26 +1,75 @@
+import argparse
+import re
 import sys
-
+import logging
 import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.io import WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions
 from ext import ReadFromBigQuery
+from apache_beam.options.pipeline_options import SetupOptions
 
-options = {
-    'project': "<project_id>",
-    'runner': 'Direct',
-    'region': "asia-south1",
-    'temp_location': "gs://dataflow-temp-storage/tmp",
-}
-# Define the pipeline options
-options = PipelineOptions(sys.argv, **options)
-pipline = beam.Pipeline(options=options)
-pipline | 'Read from BigQuery' >> ReadFromBigQuery(
-    query='SELECT * FROM `bigquery-public-data.samples.shakespeare` LIMIT 10',
-    use_standard_sql=True) | "PrintData" >> beam.Map(print)
+class WordExtractingDoFn(beam.DoFn):
+  """Parse each line of input text into words."""
+  def process(self, element):
+    """Returns an iterator over the words of this element.
 
-result = pipline.run()
-result.wait_until_finish()
+    The element is a line of text.  If the line is blank, note that, too.
 
-query_result = result.metrics().query()
+    Args:
+      element: the element being processed
 
-for counter in query_result['counters']:
-    print(f"Counter: {counter.key.metric.name}, Value: {counter.committed}")
+    Returns:
+      The processed element.
+    """
+    return re.findall(r'[\w\']+', element, re.UNICODE)
+
+def run(argv=None, save_main_session=True):
+   
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input',
+        dest='input',
+        default='gs://dataflow-samples/shakespeare/kinglear.txt',
+        help='Input file to process.')
+    parser.add_argument(
+        '--output',
+        dest='output',
+        required=True,
+        help='Output file to write results to.')
+    known_args, pipeline_args = parser.parse_known_args(argv)
+
+    # We use the save_main_session option because one or more DoFn's in this
+    # workflow rely on global context (e.g., a module imported at module level).
+    pipeline_options = PipelineOptions(pipeline_args)
+    pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
+
+    pipeline = beam.Pipeline(options=pipeline_options)
+
+    # Read the text file[pattern] into a PCollection.
+    lines = pipeline | 'Read' >> ReadFromText(known_args.input)
+
+    counts = (
+        lines
+        | 'Split' >> (beam.ParDo(WordExtractingDoFn()).with_output_types(str))
+        | 'PairWithOne' >> beam.Map(lambda x: (x, 1))
+        | 'GroupAndSum' >> beam.CombinePerKey(sum))
+
+    # Format the counts into a PCollection of strings.
+    def format_result(word, count):
+        return '%s: %d' % (word, count)
+
+    output = counts | 'Format' >> beam.MapTuple(format_result)
+
+    # Write the output using a "Write" transform that has side effects.
+    # pylint: disable=expression-not-assigned
+    output | 'Write' >> WriteToText(known_args.output)
+
+    # Execute the pipeline and return the result.
+    result = pipeline.run()
+    result.wait_until_finish()
+    return result
+
+if __name__ == '__main__':
+  logging.getLogger().setLevel(logging.INFO)
+  run()
